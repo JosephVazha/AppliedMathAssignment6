fd0bd583aec2383deb086359784cdac5780d287a
Josephvazhae1 <87318178+Josephvazhae1@users.noreply.github.com>
2025-11-18 14:00:00 -0500

    ratefunc

--- string_rate_func01.m
+++ string_rate_func01.m
@@ -16,8 +16,10 @@ function dVdt = string_rate_func01(t,V,string_params)
 
     n = string_params.n; %number of masses
     M = string_params.M; %total mass attached to the string
+
     Uf_func = string_params.Uf_func; %function describing motion of end point
     dUfdt_func = string_params.dUfdt_func; %time derivative of Uf
+
     Tf = string_params.Tf; %tension in string
     L = string_params.L; %length of string
     c = string_params.c; %damping coefficient
@@ -31,7 +33,7 @@ function dVdt = string_rate_func01(t,V,string_params)
     
     %compute acceleration
     
-    d2Udt2 = %your code here (may take several lines)
+    d2Udt2 = (Tf/dx) * (%u_(i-1) - 2u_i + u_(i+1)) 
     
     %assemble state derivative
     
