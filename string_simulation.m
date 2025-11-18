96f67352394923324ebe405269115b5f322d0f50
Josephvazhae1 <87318178+Josephvazhae1@users.noreply.github.com>
2025-11-18 13:58:54 -0500

    Initial files

---
+++ string_simulation.m
@@ -0,0 +1,42 @@
+function string_simulation(string_params, U0, dUdt0, tspan)
+
+    % num_masses = %your code here
+    % total_mass = %your code here
+    % tension_force = %your code here
+    % string_length = %your code here
+    % damping_coeff = %your code here
+    % amplitude_Uf = %your code here
+    % omega_Uf = %your code here
+
+    dx = string_length/(num_masses+1);
+    
+    %list of x points (including the two endpoints)
+    xlist = linspace(0,string_length,num_masses+2);
+    Uf_func = @(t_in) amplitude_Uf*cos(omega_Uf*t_in);
+    dUfdt_func = @(t_in) -omega_Uf*amplitude_Uf*sin(omega_Uf*t_in);
+
+    %generate the struct
+    % string_params = struct();
+    % num_masses = string_params.n;
+    % string_params.M = total_mass;
+    % string_params.Uf_func = Uf_func;
+    % string_params.dUfdt_func = dUfdt_func;
+    % string_params.Tf = tension_force;
+    % string_params.L = string_length;
+    % string_params.c = damping_coeff;
+    % string_params.dx = dx;
+
+    %load string_params into rate function
+    my_rate_func = @(t_in,V_in) string_rate_func01(t_in,V_in,string_params);
+
+    % %initial conditions
+    % U0 = %your code here
+    % dUdt0 = %your code here
+    V0 = [U0;dUdt0];
+    % tspan = %your code here
+
+    %run the integration
+    [tlist,Vlist] = your_integrator(my_rate_func,tspan,V0,...);
+    %your code to generate an animation of the system
+
+end
---
+++ stringparams1.m
@@ -0,0 +1,10 @@
+function str_params = stringparams1()
+    str_params.n = 1; %num_masses
+    str_params.M = 1; %total_mass
+    str_params.Uf_func = ;          %Uf_func
+    str_params.dUfdt_func =         %dUfdt_func
+    str_params.Tf =                 %tension_force
+    str_params.L =                  %string_length
+    str_params.c =                  %damping_coeff
+    str_params.dx =                 %dx
+end
