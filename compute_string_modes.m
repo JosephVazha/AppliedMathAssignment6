3e53d00bc8108ea1b46f6c5f2f7d4c159f921575
Addison Fisher <afisher@olin.edu>
2025-12-04 12:01:39 -0500

    possibly working modal analysis

---
+++ compute_string_modes.m
@@ -0,0 +1,20 @@
+function [mode_shapes, natural_freqs] = compute_string_modes(string_params)
+    %obtain M and K matrices
+    [M_mat, K_mat] = construct_2nd_order_matrices(string_params);
+
+    %solve for eigenvalues
+    [Ur_mat, lambda_mat] = eig(K_mat, M_mat);
+
+    omega = sqrt(abs(diag(lambda_mat)));
+
+    [natural_freqs, sort_indices] = sort(omega);
+
+    mode_shapes = Ur_mat(:, sort_indices);
+
+    %{ 
+    NEED TO DO: enerate an animation that compares the predicted mode shape to the actual result. You
+    have free artistic license here, but I would personally suggest that you have two subplots, one with the mode shape
+    and the other with the animation of the vibrating string. Up to you.
+    end
+    %}
+end
