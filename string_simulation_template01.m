637b85b609c96794105b206b9ee9ae0a4830276c
Josephvazhae1 <87318178+Josephvazhae1@users.noreply.github.com>
2025-12-05 16:32:07 -0500

    working simm

--- string_simulation_template01.m
+++ string_simulation_template01.m
@@ -1,30 +1,36 @@
 function string_simulation_template01()
-    num_masses = 3;
-    % total_mass = 1;
-    % tension_force = 10;
-    % string_length = 5;
-    % damping_coeff = .1;
-    % % 
-    % dx = string_length/(num_masses+1);
-    % 
-    % amplitude_Uf = 0.5;
-    % omega_Uf = pi/2;
-    % 
-    % %list of x points (including the two endpoints)
-    % xlist = linspace(0,string_length,num_masses+2);
-    % 
-    % Uf_func = @(t_in) amplitude_Uf*cos(omega_Uf*t_in);
-    % dUfdt_func = @(t_in) -omega_Uf*amplitude_Uf*sin(omega_Uf*t_in);
+    num_masses = 4;
+    total_mass = 4;
+    tension_force = 10;
+    string_length = 10;
+    damping_coeff = 0.1;
+    dx = string_length/(num_masses+1);
+    amplitude_Uf = 1;
+    omega_Uf = pi/2;
+
+    %list of x points (including the two endpoints)
+    xlist = linspace(0,string_length,num_masses+2);
+
+    Uf_func = @(t_in) amplitude_Uf*cos(omega_Uf*t_in);
+    dUfdt_func = @(t_in) -omega_Uf*amplitude_Uf*sin(omega_Uf*t_in);
 
     %generate the struct
-    string_params = stringparams1();
+    string_params = struct();
+    string_params.n = num_masses;
+    string_params.M = total_mass;
+    string_params.Uf_func = Uf_func;
+    string_params.dUfdt_func = dUfdt_func;
+    string_params.Tf = tension_force;
+    string_params.L = string_length;
+    string_params.c = damping_coeff;
+    string_params.dx = dx;
 
     %load string_params into rate function
     my_rate_func = @(t_in,V_in) string_rate_func01(t_in,V_in,string_params);
 
     %initial conditions
-    U0 = zeros(num_masses);
-    dUdt0 = zeros(num_masses);
+    U0 = zeros(num_masses, 1);
+    dUdt0 = zeros(num_masses, 1);
     V0 = [U0;dUdt0];
 
     tspan = [0 20];
@@ -43,8 +49,8 @@ function string_simulation_template01()
     
         u_left = 0;
         u_right = Uf_func(current_t);
-    
-        u_full = [u_left, current_U, u_right];
+        
+        u_full = [u_left; current_U; u_right];
     
         plot(xlist, u_full, '-o', 'LineWidth', 1.5, 'MarkerFaceColor', 'b');
         ylim([-amplitude_Uf*2, amplitude_Uf*2]); 
@@ -54,5 +60,6 @@ function string_simulation_template01()
         ylabel('Displacement u');
         grid on;
         drawnow;
+
     end
 end
